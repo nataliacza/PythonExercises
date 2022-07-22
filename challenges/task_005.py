abda9c1272317018ce126562e51e289a5651aac6
Natalia Czapska <czapskanatalia@gmail.com>
2022-07-22 11:56:35 +0200

    Function for task 005 implemented and tested

--- challenges/task_005.py
+++ challenges/task_005.py
@@ -1,4 +1,12 @@
 """ Create function that takes for arguments: list, index, index. Swap two index and return list. """
 
-def swap_elements(array: list, first_index: int, second_index: int) -> list:
-    pass
+def swap_elements(array: list, index_one: int, index_two: int) -> list:
+    (array[index_one], array[index_two]) = (array[index_two], array[index_one])
+    return array
+
+
+def swap_elements_2(array: list, index_one: int, index_two: int) -> list:
+    tmp = array[index_one]
+    array[index_one] = array[index_two]
+    array[index_two] = tmp
+    return array
--- tests/test_task_005.py
+++ tests/test_task_005.py
@@ -1,5 +1,5 @@
 import unittest
-from challenges.task_005 import swap_elements
+from challenges.task_005 import swap_elements, swap_elements_2
 
 list_nr1 = [4, 5, 6, 7, 1]
 list_nr2 = ["a", "b", "c", "d", "e"]
@@ -20,5 +20,23 @@ class TestOfSwapFunctionOne(unittest.TestCase):
 
     def test_function_3(self):
         result = swap_elements(list_nr3, 3, 2)
+        expected = [2.1, 5.2, 7.1, 6.2, 2.6, 2.1]
+        self.assertEqual(result, expected)
+
+
+class TestOfSwapFunctionTwo(unittest.TestCase):
+
+    def test_function_1(self):
+        result = swap_elements_2(list_nr1, 1, 2)
+        expected = [4, 6, 5, 7, 1]
+        self.assertEqual(result, expected)
+
+    def test_function_2(self):
+        result = swap_elements_2(list_nr2, 4, 2)
+        expected = ["a", "b", "e", "d", "c"]
+        self.assertEqual(result, expected)
+
+    def test_function_3(self):
+        result = swap_elements_2(list_nr3, 3, 2)
         expected = [2.1, 5.2, 7.1, 6.2, 2.6, 2.1]
         self.assertEqual(result, expected)
