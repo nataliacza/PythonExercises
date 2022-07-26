d2d080fac236ed307bf26d963a0df9853f71b31b
Natalia Czapska <czapskanatalia@gmail.com>
2022-07-26 13:21:29 +0200

    Resolved task 013

--- challenges/task_013.py
+++ challenges/task_013.py
@@ -1,4 +1,11 @@
 """ Define a function that takes for argument nested list and sort elements ascending by each nested list. """
 
 def sort_by_row(array: list) -> list:
-    pass
+    for index, value in enumerate(array):
+        array[index] = sorted(array[index])
+    return array
+
+def sort_by_row_2(array: list) -> list:
+    for item in array:
+        item.sort()
+    return array
--- tests/test_task_013.py
+++ tests/test_task_013.py
@@ -1,5 +1,5 @@
 import unittest
-from challenges.task_013 import sort_by_row
+from challenges.task_013 import sort_by_row, sort_by_row_2
 
 list1 = [[4, 7, 2, 7, 9, 1], [6, 3, 2, 8, 8], [9, 7, 3, 2, 7]]
 expected1 = [[1, 2, 4, 7, 7, 9], [2, 3, 6, 8, 8], [2, 3, 7, 7, 9]]
@@ -16,5 +16,18 @@ class TestOfSortFunctionOne(unittest.TestCase):
 
     def test_function_2(self):
         result = sort_by_row(list2)
+        expected = expected2
+        self.assertEqual(result, expected)
+
+
+class TestOfSortFunctionTwo(unittest.TestCase):
+
+    def test_function_1(self):
+        result = sort_by_row_2(list1)
+        expected = expected1
+        self.assertEqual(result, expected)
+
+    def test_function_2(self):
+        result = sort_by_row_2(list2)
         expected = expected2
         self.assertEqual(result, expected)
