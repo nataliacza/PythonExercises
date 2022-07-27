a76f07d95a378bdf7fc5e6a06ec296cb5f9bce72
Natalia Czapska <czapskanatalia@gmail.com>
2022-07-27 12:43:23 +0200

    Task 017 resolved.

--- challenges/task_017.py
+++ challenges/task_017.py
@@ -1,5 +1,10 @@
 """ Define a function that takes for arguments list and integer k. Function should return all elements from the list,
  that are k away from value. Skip first and last element of the list in result. """
 
-def calculate(array: list, value: int = 5) -> list:
-    pass
+def calculate(array: list, k: int = 5) -> list:
+    result = []
+    for index in range(1, len(array) - 1):
+        if abs(array[index - 1] - array[index]) >= k \
+                and abs(array[index + 1] - array[index]) >= k:
+            result.append(array[index])
+    return result
